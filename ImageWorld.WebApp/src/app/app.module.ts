3c28454e4bf30065dd9901d4f36c756a6b377aa6
Tim Scarfe <tim+git@developer-x.com>
2017-06-09 10:04:31 +0100

    Fixed http module error

--- ImageWorld.WebApp/src/app/app.module.ts
+++ ImageWorld.WebApp/src/app/app.module.ts
@@ -8,7 +8,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+      BrowserModule,
+      HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
