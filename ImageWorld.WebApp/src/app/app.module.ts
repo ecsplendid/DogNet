5d7e606cfef597b02bce481ae224912795030c34
Tim Scarfe <tim+git@developer-x.com>
2017-06-10 07:14:04 +0100

    Empty file upload component (works)

--- ImageWorld.WebApp/src/app/app.module.ts
+++ ImageWorld.WebApp/src/app/app.module.ts
@@ -2,22 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
 import { AppComponent, galleryConfig } from './gallery/app.component';
-
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { GalleryModule } from 'ng-gallery';
+import { SimpleFileUploader } from './fileupload/fupload.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SimpleFileUploader
   ],
   imports: [
     BrowserModule,
     HttpModule,
     BrowserAnimationsModule,
-    GalleryModule.forRoot(galleryConfig)
+    GalleryModule.forRoot(galleryConfig),
+    
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent, SimpleFileUploader]
 })
 export class AppModule { }
---
+++ ImageWorld.WebApp/src/app/fileupload/fupload.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'file-uploader',
+  templateUrl: './fupload.component.html'
+})
+export class SimpleFileUploader {
+
+}
